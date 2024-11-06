4d2395addac07cb98c5215571921423f9a3f4829
Sandy@007 <sandrasaju07@gmail.com>
2024-11-06 11:36:18 +0530

    Made some changes

--- Hello_World/app.py
+++ Hello_World/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,redirect,url_for
 
 app = Flask(__name__)   # Creating WSGI Application
 
@@ -6,5 +6,22 @@ app = Flask(__name__)   # Creating WSGI Application
 def welcome():
     return 'Hello, Welcome to the Page!!'
 
+@app.route('/success/<name>')
+def login_success(name):
+    return " You have successfully logged in, " + name
+
+@app.route('/fail/<name>')
+def login_fail(name):
+    return "Sorry " + name + ", log in failed, please try again!"
+
+@app.route('/login/<username>')
+def login_check(username):
+    page = ""
+    if username == 'sandra':
+        page = 'login_success'
+    else:
+        page = 'login_fail'
+    return redirect(url_for(page,name=username))
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
